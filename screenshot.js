a2c92f3b8d7f924bc8498bb0449f189f48f52583
kljdfin <alan.sisel@stantec.com>
2025-01-13 12:59:04 -0700

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -7,31 +7,24 @@ function takeScreenshotAndRedirect() {
         height: 600 // Replace with your desired height
     };
 
-    // Create a new canvas to capture the image
-    const captureCanvas = document.createElement('canvas');
-    const captureCtx = captureCanvas.getContext('2d');
-
-    // Set dimensions to match the capture area
-    captureCanvas.width = captureArea.width;
-    captureCanvas.height = captureArea.height;
-
-    // Draw the specified area of the screen onto the capture canvas
+    // Use html2canvas to capture the specified area of the screen
     html2canvas(document.body, {
         x: captureArea.x,
         y: captureArea.y,
         width: captureArea.width,
         height: captureArea.height
     }).then(canvas => {
-        captureCtx.drawImage(canvas, captureArea.x, captureArea.y, captureArea.width, captureArea.height, 0, 0, captureArea.width, captureArea.height);
-
         // Convert canvas to data URL (image)
-        const screenshotData = captureCanvas.toDataURL('image/png');
+        const screenshotData = canvas.toDataURL('image/png');
 
         // Optionally download the image
         const link = document.createElement('a');
         link.href = screenshotData;
         link.download = 'screenshot.png'; // Set filename
         link.click();
+
+        // Redirect to Google.com
+        window.location.href = 'https://www.google.com';
     });
 }
 
